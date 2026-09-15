26697bcd06a5ef6a9621817e523dda34fc617ae0
Subachev Denis <subachevdenis@yandex.ru>
2026-09-15 11:41:08 +0000

    ci: ссылка на исходный код, base-путь /tools/erd2/, публикация образа в GHCR

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,7 @@ import UnpluginInjectPreload from 'unplugin-inject-preload/vite';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+    base: '/tools/erd2/',
     plugins: [
         react(),
         visualizer({
